56958ed5db90d8a96c7d5dc8daa1ae5c02a34fce
sekthor <sysdroyed@gmail.com>
2021-05-23 14:44:46 +0200

    print version number

--- src/main.c
+++ src/main.c
@@ -1,15 +1,44 @@
 #include <stdio.h>
+#include <string.h>
+#include "main.h"
 
-#define MIN_ARGS 2
-#define ERROR_TOO_FEW_ARGUMENTS 1
 
 int main(int argc, char **argv)
 {
     if (argc < MIN_ARGS) {
         fprintf(stderr, "ERROR: Not enough arguments. Got %d, expected at least %d\n", argc - 1, MIN_ARGS - 1);
-        return ERROR_TOO_FEW_ARGUMENTS;
+        return EXIT_TOO_FEW_ARGUMENTS;
+    }
+
+    for (int i = 1; i < argc; i++) {
+
+        if (!strcmp(argv[i], "--help") || !strcmp(argv[i], "-h")) {
+            print_help();
+            return EXIT_SUCCESS;
+        }
+            
+        if (!strcmp(argv[i], "--version") || !strcmp(argv[i], "-v")) {
+            print_version();
+            return EXIT_SUCCESS;
+        }
     }
     
     printf("Hello %s\n", argv[1]);
-    return 0;
+    return EXIT_SUCCESS;
+}
+
+void print_help()
+{
+    printf("Usage:\n"
+        "main argument\n\n"
+        "ARGUMENTS:\n"
+        "----------\n"
+        "-h  --help    print this menu\n"
+        "-v  --version print version number\n");
+}
+
+void print_version()
+{
+    puts(VERSION);
+    exit(EXIT_TOO_FEW_ARGUMENTS);
 }
