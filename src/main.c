f28d6079900804874cd15b2ca7ee7a0952fc4c6f
sekthor <sysdroyed@gmail.com>
2021-05-23 14:05:35 +0200

    check number of arguments

--- src/main.c
+++ src/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int main()
+#define MIN_ARGS 2
+#define ERROR_TOO_FEW_ARGUMENTS 1
+
+int main(int argc, char **argv)
 {
-    printf("Hello World\n");
+    if (argc < MIN_ARGS) {
+        fprintf(stderr, "ERROR: Not enough arguments. Got %d, expected at least %d\n", argc - 1, MIN_ARGS - 1);
+        return ERROR_TOO_FEW_ARGUMENTS;
+    }
+    
+    printf("Hello %s\n", argv[1]);
     return 0;
 }
